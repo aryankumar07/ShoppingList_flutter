314a9c012f8e8cbebee09a8489c85977dc258ad7
aryan kumar <ak2839426@gmail.com>
2023-10-07 14:53:11 +0530

    adding firebase

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:slist/data/dummy_items.dart';
+import 'package:slist/models/grocery_item.dart';
 import 'package:slist/widgets/new_item.dart';
 
 class GroceryList extends StatefulWidget {
@@ -11,14 +10,66 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void _addItem(){
-    Navigator.of(context).push(
+
+
+ final List<GroceryItem> groceryItems = []; 
+
+
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => NewItem()));
+
+        if(newItem==null){
+          return;
+        }
+
+        setState(() {
+          groceryItems.add(newItem);
+        });
   }
+
+  void _removeItem(GroceryItem item){
+    setState(() {
+      groceryItems.remove(item);
+    });
+  }
+
+  
 
   @override
   Widget build(BuildContext context) {
+
+    Widget activeWidget = Scaffold(
+      body: Center(
+        child: Text("Nothing to show here! \n try adding something"),
+      ),
+    );
+
+  if(groceryItems.length>0){
+    activeWidget = ListView.builder(
+          itemCount: groceryItems.length,
+          itemBuilder: (ctx, index) => Dismissible(
+            onDismissed: (direction) {
+              _removeItem(groceryItems[index]);
+            },
+            key: ValueKey(groceryItems[index].id),
+            child: ListTile(
+                title: Text(groceryItems[index].name),
+                leading: Container(
+                  width: 24,
+                  height: 24,
+                  color: groceryItems[index].category.selectedColor,
+                ),
+                trailing: Text(
+                  groceryItems[index].quantity.toString(),
+                ),
+              ),
+          ),
+    );
+  }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
@@ -29,20 +80,7 @@ class _GroceryListState extends State<GroceryList> {
             )
         ],
       ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: groceryItems[index].category.selectedColor,
-          ),
-          trailing: Text(
-            groceryItems[index].quantity.toString(),
-          ),
-        ),
-      ),
+      body: activeWidget,
     );
   }
 }
